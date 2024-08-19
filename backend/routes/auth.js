1cd127a4c52eadd3f4f1f5c00359deb34c9dcce5
Sahil Khan <95959159+sahilkhan786000@users.noreply.github.com>
2024-08-19 12:26:50 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -53,20 +53,4 @@ router.post('/login', async (req, res) => {
 });
 
 
-
-
-// Middleware to verify the token
-const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ error: 'No token, authorization denied' });
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
-    next();
-  } catch (error) {
-    res.status(401).json({ error: 'Invalid token' });
-  }
-};
-
-module.exports = { router, verifyToken };
+module.exports = { router};
